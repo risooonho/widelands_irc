48ddc49f70b2d3b69164055c3f9e0fa6d90ba78c
Janus <janus@widelands.org>
2019-04-14 18:55:24 +0200

    add x-hub-signature verification

--- src/bot.py
+++ src/bot.py
@@ -4,6 +4,8 @@ import events
 import sys
 import threading
 import config
+import hmac
+import hashlib
 from irc import IrcConnection
 
 irc = None
@@ -21,9 +23,9 @@ class MyHandler(BaseHTTPRequestHandler):
     def do_CONNECT(self):
         pass
     def do_POST(self):
-        print(self.headers)
         if not all(x in self.headers for x in [CONTENT_TYPE, CONTENT_LEN, EVENT_TYPE]):
             return
+
         content_type = self.headers['content-type']
         content_len = int(self.headers['content-length'])
         event_type = self.headers['x-github-event']
@@ -31,6 +33,12 @@ class MyHandler(BaseHTTPRequestHandler):
         data = self.rfile.read(content_len)
         if sys.version_info < (3, 6):
             data = data.decode()
+
+        if irc.widelands['webhook']['secret'] and self.headers['x-hub-signature']:
+            github_signature = self.headers['x-hub-signature'].split('=')[1]
+            verify_signature = _generate_signature(data)
+            if github_signature != verify_signature:
+                return
 
         self.send_response(200)
         self.send_header('content-type', 'text/html')
@@ -44,6 +52,12 @@ def worker():
     irc.loop()
 
 irc = IrcConnection('src/config.ini')
+
+def _generate_signature(data):
+    key = irc.widelands['webhook']['secret']
+    key_bytes= bytes(key , 'utf-8')
+    data_bytes = bytes(data, 'utf-8')
+    return hmac.new(key_bytes, data_bytes , hashlib.sha1).hexdigest()
 
 t = threading.Thread(target=worker)
 t.start()
